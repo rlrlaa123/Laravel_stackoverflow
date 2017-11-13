6d51c0afeeca37917d4eb32edff22545ffb497dd
rlrlaa123 <kimdonghyun3366@gmail.com>
2017-11-13 21:12:21 +0900

    development planning

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+//        flash('환영합니다.');
+
+        flash()->success('성공했습니다.');
+//        flash('성공했습니다.', 'success');
+
+//        flash()->warning('경고! 누구세요?');
+//        flash('경고! 누구세요?', 'warning');
+
+//        flash()->danger('오류가 발생했습니다.');
+//        flash('오류가 발생했습니다.', 'danger');
+
+        return view('home');
+    }
+}
---
+++ config/project.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: donghyunkim
+ * Date: 2017. 11. 13.
+ * Time: PM 8:26
+ */
+
+return [
+    'name' => 'Stackoverflow',
+    'url' => 'http://myapp.dev:8000',
+    'description'=>'idk',
+];
